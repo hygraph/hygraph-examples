b1bc46d4f82dc32a7638367b22555649eb66ff92
hmeissner <hugomeissner@icloud.com>
2017-11-29 20:39:42 +0100

    cra temp fix for multiple authors

--- createReactApp-apollo-blog/src/components/AboutPage/AboutPage.js
+++ createReactApp-apollo-blog/src/components/AboutPage/AboutPage.js
@@ -8,19 +8,23 @@ import './AboutPage.css'
 const AboutPage = ({ data: { loading, error, allAuthors } }) => {
   if (error) return <ErrorMessage message='Error loading author.' />
   if (!loading) {
-    return allAuthors.map(author => (
-      <div key='author.id'>
-        <div className='AboutPage-infoHeader'>
-          <img
-            className='AboutPage-img'
-            alt={author.name}
-            src={`https://media.graphcms.com/resize=w:100,h:100,fit:crop/${author.avatar.handle}`}
-          />
-          <h1>Hello! My name is {author.name}</h1>
-        </div>
-        <p>{author.bibliography}</p>
+    return (
+      <div>
+        {allAuthors.map(author => (
+          <div key='author.id'>
+            <div className='AboutPage-infoHeader'>
+              <img
+                className='AboutPage-img'
+                alt={author.name}
+                src={`https://media.graphcms.com/resize=w:100,h:100,fit:crop/${author.avatar.handle}`}
+              />
+              <h1>Hello! My name is {author.name}</h1>
+            </div>
+            <p>{author.bibliography}</p>
+          </div>
+        ))}
       </div>
-    ))
+    )
   }
   return <h2>Loading author...</h2>
 }
