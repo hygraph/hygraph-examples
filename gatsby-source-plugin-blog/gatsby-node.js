528ba312be336ff96d74c41e0a9c15f24e4b308e
Hugo Meissner <hugomeissner@icloud.com>
2017-12-02 01:31:23 +0100

    (╯°□°）╯︵ ┻━┻

--- gatsby-source-plugin-blog/gatsby-node.js
+++ gatsby-source-plugin-blog/gatsby-node.js
@@ -40,21 +40,3 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
     })
   })
 }
-
-// we create markdown nodes here so transformer-remark can process them
-exports.onCreateNode = ({ node, boundActionCreators }) => {
-  const { createNode } = boundActionCreators
-  if (node.internal.type === `Posts`) {
-    createNode({
-      id: `md-${node.id}`,
-      parent: node.id,
-      children: [],
-      internal: {
-        type: `${node.internal.type}Markdown`,
-        mediaType: `text/markdown`,
-        content: node.content,
-        contentDigest: node.internal.contentDigest
-      }
-    })
-  }
-}
