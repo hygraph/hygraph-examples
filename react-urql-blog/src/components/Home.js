c5362b59a7a6a9708d369151e9c0ccc481488028
hmeissner <hugomeissner@icloud.com>
2018-02-19 11:20:13 +0100

    urql pagination

--- react-urql-blog/src/components/Home.js
+++ react-urql-blog/src/components/Home.js
@@ -2,47 +2,72 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Connect, query } from 'urql'
 
-const Home = () => (
-  <Connect
-    query={query(Posts)}
-    children={({ loaded, data, error}) => {
-      if (error) return <div>Error!</div>
-      if (!loaded) return <div>Loading...</div>
+const POSTS_PER_PAGE = 4
 
-      return (
-      <section>
-        <ul className='Home-ul'>
-          {data.allPosts.map(post => (
-            <li className='Home-li' key={`post-${post.id}`}>
-              <Link to={`/post/${post.slug}`} className='Home-link'>
-                <div className='Home-placeholder'>
-                  <img
-                    alt={post.title}
-                    className='Home-img'
-                    src={`https://media.graphcms.com/resize=w:100,h:100,fit:crop/${post.coverImage.handle}`}
-                  />
-                </div>
-                <h3>{post.title}</h3>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </section>
+class Home extends React.Component {
+  state = {
+    first: 4
+  }
+
+  render() {
+    return (
+      <Connect
+        query={query(Posts, { first: this.state.first })}
+        children={({ loaded, data, error, refetch }) => {
+          if (error) return <div>Error!</div>
+          if (loaded) {
+            const {allPosts, _allPostsMeta} = data
+            const areMorePosts = allPosts.length < _allPostsMeta.count
+            return (
+            <section>
+              <ul className='Home-ul'>
+                {allPosts.map(post => (
+                  <li className='Home-li' key={`post-${post.id}`}>
+                    <Link to={`/post/${post.slug}`} className='Home-link'>
+                      <div className='Home-placeholder'>
+                        <img
+                          alt={post.title}
+                          className='Home-img'
+                          src={`https://media.graphcms.com/resize=w:100,h:100,fit:crop/${post.coverImage.handle}`}
+                        />
+                      </div>
+                      <h3>{post.title}</h3>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+              <div className='Home-showMoreWrapper'>
+              {areMorePosts
+                ? <button className='Home-button' onClick={() => this.setState(({ first }) => ({ first: first + POSTS_PER_PAGE }))}>
+                  {!loaded ? 'Loading...' : 'Load next page'}
+                </button>
+                : ''}
+              </div>
+            </section>
+          )
+          }
+          return <div>Loading...</div>
+        }
+      } />
     )
-    }
-  } />
-)
+  }
+}
 
-export const Posts = `{
-  allPosts(orderBy: dateAndTime_DESC) {
-    id
-    slug
-    title
-    dateAndTime
-    coverImage {
-      handle
+export const Posts = `
+  query allPosts($first: Int!) {
+    allPosts(orderBy: dateAndTime_DESC, first: $first) {
+      id
+      slug
+      title
+      dateAndTime
+      coverImage {
+        handle
+      }
+    }
+    _allPostsMeta {
+        count
     }
   }
-}`
+`
 
 export default Home
