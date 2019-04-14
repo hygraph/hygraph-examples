7b728d1515f6a00c3ea2a414be741db4c05ebabb
Fabian Beliza <fabianbeliza+github@icloud.com>
2019-04-14 21:44:36 +0200

    Migrate more

--- current/gatsby-source-plugin-blog/src/components/Header.js
+++ current/gatsby-source-plugin-blog/src/components/Header.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import { Link } from 'gatsby';
 
-import styles from '../styles/header.module.css'
+import styles from '../styles/header.module.css';
 
 const Header = () => (
   <header className={styles.header}>
     <h1 className={styles.h1}>GraphCMS Starter blog</h1>
     <nav className={styles.nav}>
-      <Link to='/' exact className={styles.link} activeClassName={styles.active}>
+      <Link
+        to="/"
+        exact
+        className={styles.link}
+        activeClassName={styles.active}
+      >
         Home
       </Link>
-      <Link to='/about' className={styles.link} activeClassName={styles.active}>
+      <Link to="/about" className={styles.link} activeClassName={styles.active}>
         About
       </Link>
     </nav>
   </header>
-)
+);
 
-export default Header
+export default Header;
--- current/gatsby-source-plugin-blog/src/pages/index.js
+++ current/gatsby-source-plugin-blog/src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from 'gatsby-link';
+import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import { graphql } from 'gatsby';
 
