dd52e1483563c1f7177a2241215a5ce71c4af701
Jesse <themotleydev@gmail.com>
2020-04-17 14:27:30 +0200

    Initial Push

---
+++ next/vue-starter/src/views/Product.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="product" v-if="product">
+    <h1>{{ product.name }}</h1>
+    <p>{{ product.description }}</p>
+    <p>
+      {{
+        new Intl.NumberFormat('de-DE', {
+          style: 'currency',
+          currency: 'EUR',
+        }).format(product.price / 100)
+      }}
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      product: null,
+      error: null,
+      loading: true,
+    };
+  },
+  created() {
+    // fetch the data when the view is created and the data is
+    // already being observed
+    this.fetchData();
+  },
+  watch: {
+    // call again the method if the route changes
+    $route: 'fetchData',
+  },
+  methods: {
+    async fetchData() {
+      this.error = null;
+      this.post = null;
+      this.loading = true;
+      // replace `getPost` with your data fetching util / API wrapper
+      try {
+        const response = await fetch(process.env.VUE_APP_GRAPHCMS_ENDPOINT, {
+          method: 'POST',
+          body: JSON.stringify({
+            query: `query GetProduct($slug: String){ product(where: {slug: $slug}) { name description price } }`,
+            variables: {
+              slug: this.$route.params.slug,
+            },
+          }),
+        });
+        const { data } = await response.json();
+        this.loading = false;
+        this.error = data.error;
+        this.product = data.product;
+      } catch (e) {
+        // handle error
+      }
+    },
+  },
+};
+</script>
