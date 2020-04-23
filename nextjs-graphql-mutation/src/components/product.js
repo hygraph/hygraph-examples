126bd15a0337b87c85b9d4c0b7f1395e5eeafd15
Jonathan Steele <jonathan@steele.pro>
2020-04-23 06:35:30 +0100

    fix: Ensure we run query aggregate query for all products
    Pass id as an argument, multipel dependency signature

--- nextjs-graphql-mutation/src/components/product.js
+++ nextjs-graphql-mutation/src/components/product.js
@@ -2,15 +2,18 @@ import { request } from 'graphql-request';
 import useSWR from 'swr';
 
 function Product({ id, name }) {
-  const { data } = useSWR(
-    `query productVotesCount($id: ID!) {
-      productVotes: votesConnection(where: {product: {id: $id}}) {
-        aggregate {
-          count
+  const { data, mutate } = useSWR(
+    [
+      `query productVotesCount($id: ID!) {
+        productVotes: votesConnection(where: {product: {id: $id}}) {
+          aggregate {
+            count
+          }
         }
-      }
-    }`,
-    (query) =>
+      }`,
+      id,
+    ],
+    (query, id) =>
       request(
         'https://api-eu-central-1.graphcms.com/v2/ck8sn5tnf01gc01z89dbc7s0o/master',
         query,
