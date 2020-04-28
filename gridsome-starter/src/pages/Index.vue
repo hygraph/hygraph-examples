fb2c13eaba74a4a7dd3e0af476a311e5471d2370
Jesse <themotleydev@gmail.com>
2020-04-28 16:43:29 +0200

    Gridsome readme update (#136)
    * add readme and env sample
    
    * add env.sample
    
    * update now.json
    
    * fix readme
    
    * add codesandbox
    
    * test now.json
    
    * removed process variables
    
    * removed process variables changed url
    
    * more hosting targets
    
    * remove hosting targets
    
    * add gridsome logo
    
    * tweak formatting
    
    * tweak formatting

--- gridsome-starter/src/pages/Index.vue
+++ gridsome-starter/src/pages/Index.vue
@@ -1,6 +1,5 @@
 <template>
   <Layout>
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
     <h1>Hello, world!</h1>
