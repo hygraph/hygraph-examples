9a7d4349221d1ac3e73589168ef34a86a8a8122a
Jonathan Steele <jonathan@steele.pro>
2020-05-01 12:10:42 +0100

    feat: Working React.js example
    Basic data fetching with graphql-request and react-router

--- with-reactjs/src/App.js
+++ with-reactjs/src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { request } from 'graphql-request';
+
+import Product from './Product';
 
 function App() {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { products } = await request(
+        'https://api-eu-central-1.graphcms.com/v2/ck8sn5tnf01gc01z89dbc7s0o/master',
+        `
+      { 
+        products {
+          id
+          name
+          slug
+        }
+      }
+    `
+      );
+
+      setProducts(products);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        {!products ? (
+          'Loading'
+        ) : (
+          <React.Fragment>
+            <ul>
+              {products.map(({ id, name, slug }) => (
+                <li key={id}>
+                  <Link to={`/products/${slug}`}>{name}</Link>
+                </li>
+              ))}
+            </ul>
+            <Switch>
+              <Route path="/products/:slug">
+                <Product products={products} />
+              </Route>
+            </Switch>
+          </React.Fragment>
+        )}
+      </Router>
     </div>
   );
 }
