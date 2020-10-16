d1f4e598d00b328c8a8ce7ff8da5a45ee2e9d7e3
Jonathan Steele <jonathan@steele.pro>
2020-10-16 12:17:22 +0100

    fix: Add missing key attr

--- with-gatsby-filesystem-routing/src/pages/index.js
+++ with-gatsby-filesystem-routing/src/pages/index.js
@@ -3,14 +3,17 @@ import { graphql, Link } from 'gatsby';
 
 function IndexPage({ data: { products } }) {
   return products.nodes.map((product) => (
-    <Link to={product.gatsbyPath}>{product.name}</Link>
+    <Link key={product.id} to={product.gatsbyPath}>
+      {product.name}
+    </Link>
   ));
 }
 
 export const pageQuery = graphql`
-  query IndexPageQuery {
+  {
     products: allGraphCmsProduct {
       nodes {
+        id
         name
         gatsbyPath(filePath: "/{GraphCmsProduct.slug}")
       }
