8af3ba716f62b74c4bf71ef41cf378add00ef8ad
Jonathan Steele <jonathan@steele.pro>
2020-11-16 18:49:39 +0000

    feat: Add i18n example with Next.js routing

---
+++ with-nextjs-i18n-routing/lib/graphCmsClient.js
@@ -0,0 +1,5 @@
+import { GraphQLClient } from 'graphql-request';
+
+export default new GraphQLClient(
+  'https://api-eu-central-1.graphcms.com/v2/ck8sn5tnf01gc01z89dbc7s0o/master'
+);
---
+++ with-nextjs-i18n-routing/pages/[id].js
@@ -0,0 +1,55 @@
+import { gql } from 'graphql-request';
+
+import graphCmsClient from '../lib/graphCmsClient';
+
+function ProductPage({ product }) {
+  return <pre>{JSON.stringify(product, null, 2)}</pre>;
+}
+
+export async function getStaticPaths({ locales }) {
+  let paths = [];
+
+  const { products } = await graphCmsClient.request(gql`
+    {
+      products {
+        id
+      }
+    }
+  `);
+
+  for (const locale of locales) {
+    paths = [
+      ...paths,
+      ...products.map((product) => ({ params: { id: product.id }, locale })),
+    ];
+  }
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ locale, params }) {
+  const { product } = await graphCmsClient.request(
+    gql`
+      query ProductPageQuery($id: ID!, $locale: Locale!) {
+        product(where: { id: $id }, locales: [$locale]) {
+          id
+          locale
+          name
+          slug
+        }
+      }
+    `,
+    { id: params.id, locale }
+  );
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
+
+export default ProductPage;
