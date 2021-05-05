625eb46aa983ed46c8055964b636bb7f4dee37d9
Scott Spence <yo@scottspence.com>
2021-05-05 15:12:30 +0100

    feat: add SveltKit example using graphql-request (#179)
    * feat: :sparkles: add SveltKit example using graphql-request
    
    use SveltKit based off of the Sapper example using graphql-request to fetch data
    
    * chore: :wrench: remove .gitignore
    
    * chore: :wrench:
    
    * chore: :fire: remove dotfiles
    
    * chore: :memo: Update README
    
    Add SvelteKit to README
    
    * chore: :memo: Update README links
    
    Move links ito bottom of document to have a more manageable table
    
    * chore: :package: Add Vercel adapter
    
    * chore: :package: Update project name

---
+++ with-sveltekit/svelte.config.js
@@ -0,0 +1,12 @@
+/** @type {import('@sveltejs/kit').Config} */
+import vercel from '@sveltejs/adapter-vercel';
+
+const config = {
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    adapter: vercel(),
+  },
+};
+
+export default config;
