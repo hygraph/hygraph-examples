d3502c0c75d48346f34f809d7c4b5068b4e159d0
Jamie Barton <jamie@notrab.dev>
2021-05-12 14:53:45 +0100

    initial commit

---
+++ using-rich-text-react-renderer/pages/products/[slug].js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { GraphQLClient } from 'graphql-request';
+import { RichText } from '@graphcms/rich-text-react-renderer';
+
+const graphcms = new GraphQLClient(
+  'https://api-eu-central-1.graphcms.com/v2/ck8sn5tnf01gc01z89dbc7s0o/master'
+);
+
+export async function getStaticProps({ params }) {
+  const { product } = await graphcms.request(
+    `
+    query ProductPageQuery($slug: String!) {
+      product(where: { slug: $slug }) {
+        name
+        content {
+          raw
+        }
+      }
+    }
+  `,
+    {
+      slug: params.slug,
+    }
+  );
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const { products } = await graphcms.request(`
+    {
+      products {
+        slug
+      }
+    }
+  `);
+
+  return {
+    paths: products.map(({ slug }) => ({
+      params: { slug },
+    })),
+    fallback: false,
+  };
+}
+
+const ProductPage = ({ product }) => (
+  <React.Fragment>
+    <h1>{product.name}</h1>
+
+    <RichText content={product.content.raw.children} />
+  </React.Fragment>
+);
+
+export default ProductPage;
