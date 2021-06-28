28fd2ad677b73d2b964c7100c018a00441b02e95
Scott Spence <spences10apps@gmail.com>
2021-06-28 09:55:26 +0100

    chore: :wrench: change package name (#195)

---
+++ using-management-sdk/README.md
@@ -0,0 +1,47 @@
+# Working with the GraphCMS Management SDK
+
+[Join our Slack](https://slack.graphcms.com)
+
+This example demonstrates how to programmatically build a project schema using the [GraphCMS Management SDK](https://www.npmjs.com/package/@graphcms/management).
+
+It creates the following models and field types:
+
+**Blog Post**
+
+- Title
+- Excerpt (with multi line form renderer)
+- Content as Rich Text
+- Author (Linked to Author)
+
+**Author**
+
+- Name
+- Posts (one to many)
+
+## How to Use
+
+### Download Manually
+
+```bash
+npx degit graphcms/graphcms-examples/using-management-sdk using-management-sdk
+```
+
+Add .env:
+
+```bash
+cp .env.sample .env
+```
+
+Provide values for `GRAPHCMS_MIGRATION_TOKEN` and `GRAPHCMS_URL`.
+
+Install & Run:
+
+```bash
+cd using-management-sdk
+npm install
+npm run start
+# or
+cd using-management-sdk
+yarn
+yarn start
+```
---
+++ using-management-sdk/src/index.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+
+const {
+  newMigration,
+  FieldType,
+  RelationType,
+  Renderer,
+} = require('@graphcms/management');
+
+// Create a new `migration` instance
+const migration = newMigration({
+  authToken: process.env.GRAPHCMS_MIGRATION_TOKEN,
+  endpoint: process.env.GRAPHCMS_URL,
+});
+
+// create model for blog
+const blogModel = migration.createModel({
+  apiId: 'Blog',
+  apiIdPlural: 'Blogs',
+  displayName: 'Blog',
+});
+
+// add blog fields
+blogModel.addSimpleField({
+  apiId: 'title',
+  displayName: 'Title',
+  type: FieldType.String,
+});
+
+blogModel.addSimpleField({
+  apiId: 'excerpt',
+  displayName: 'Excerpt',
+  type: FieldType.String,
+  formRenderer: Renderer.MultiLine,
+});
+
+blogModel.addSimpleField({
+  apiId: 'content',
+  displayName: 'Content',
+  type: FieldType.Richtext,
+});
+
+// create model for author
+const authorModel = migration.createModel({
+  apiId: 'Author',
+  apiIdPlural: 'Authors',
+  displayName: 'Author',
+});
+
+// add author fields
+authorModel.addSimpleField({
+  apiId: 'name',
+  displayName: 'Name',
+  type: FieldType.String,
+});
+
+// add relation to blog for author posts
+
+// create model for author
+const authorRelation = migration.updateModel({
+  apiId: 'Author',
+});
+
+authorRelation.addRelationalField({
+  apiId: 'authorPosts',
+  displayName: 'Author Posts',
+  relationType: RelationType.OneToMany,
+  model: 'Blog',
+});
+
+migration.run();
