a72a53d3757b5c4b06d97bb0d967bc89f66eefb1
Jamie Barton <jamie@notrab.dev>
2021-08-10 08:34:51 +0100

    remove hardcoded values

--- uix-cloudinary-input/src/App.tsx
+++ uix-cloudinary-input/src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useLayoutEffect, useState, useEffect } from "react";
-import { Image, Video, Placeholder } from "cloudinary-react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import React, { useLayoutEffect, useState, useEffect } from 'react';
+import { Image, Video, Placeholder } from 'cloudinary-react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import {
   Wrapper as ExtensionWrapper,
   useUiExtensionDialog,
@@ -8,7 +8,7 @@ import {
   FieldExtensionType,
   FieldExtensionFeature,
   FieldExtensionDeclaration,
-} from "@graphcms/uix-react-sdk";
+} from '@graphcms/uix-react-sdk';
 
 type Media = {
   public_id: string;
@@ -16,7 +16,7 @@ type Media = {
 };
 
 function isMediaType(item: any): item is Media {
-  return Boolean(item) && "public_id" in item && "resource_type" in item;
+  return Boolean(item) && 'public_id' in item && 'resource_type' in item;
 }
 function isMediaList(media: Media | Media[] | string): media is Media[] {
   return (
@@ -29,14 +29,14 @@ function isMediaList(media: Media | Media[] | string): media is Media[] {
 // 1. The return value type, will be null or undefined if nothing is returned; falls back to any
 type DialogReturn = Media | Media[] | null;
 // 2. Optional props you'd like to access in the dialog
-type DialogProps = { isList: boolean; media: Media | Media[] | "" };
+type DialogProps = { isList: boolean; media: Media | Media[] | '' };
 
 // Typing the declaration will ensure correct types for the values returned by useUiExtension
 const declaration: FieldExtensionDeclaration = {
-  extensionType: "field",
+  extensionType: 'field',
   fieldType: FieldExtensionType.JSON,
-  name: "Cloudinary asset",
-  description: "Pick asset object in Cloudinary",
+  name: 'Cloudinary asset',
+  description: 'Pick asset object in Cloudinary',
   features: [
     // Enables rendering of a form field
     FieldExtensionFeature.FieldRenderer,
@@ -49,13 +49,13 @@ const declaration: FieldExtensionDeclaration = {
   // Optional fields that will when adding or updating a UI extension in GraphCMS
   config: {
     CLOUD_NAME: {
-      displayName: "Cloudinary cloud name",
-      type: "string",
+      displayName: 'Cloudinary cloud name',
+      type: 'string',
       required: true,
     },
     API_KEY: {
-      displayName: "Cloudinary public api key",
-      type: "string",
+      displayName: 'Cloudinary public api key',
+      type: 'string',
       required: true,
     },
   },
@@ -120,33 +120,33 @@ function FormFieldRenderer() {
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "flex-start",
-        padding: "10px",
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        padding: '10px',
         opacity: isTransitioning ? 0 : 1,
       }}
     >
       {isExpanded ? null : (
         <button
           style={{
-            cursor: "pointer",
-            marginBottom: "10px",
-            boxSizing: "border-box",
-            userSelect: "none",
-            color: "#6663FD",
-            backgroundColor: "#F2F1FF",
-            textAlign: "center",
-            lineHeight: "16px",
-            display: "inline-flex",
-            border: "0px",
-            borderRadius: "4px",
+            cursor: 'pointer',
+            marginBottom: '10px',
+            boxSizing: 'border-box',
+            userSelect: 'none',
+            color: '#6663FD',
+            backgroundColor: '#F2F1FF',
+            textAlign: 'center',
+            lineHeight: '16px',
+            display: 'inline-flex',
+            border: '0px',
+            borderRadius: '4px',
             fontWeight: 600,
             fontFamily:
               'Inter, -apple-system, system-ui, "Segoe UI", Helvetica, Arial, sans-serif',
-            fontSize: "14px",
-            verticalAlign: "middle",
-            padding: "8px",
+            fontSize: '14px',
+            verticalAlign: 'middle',
+            padding: '8px',
           }}
           onClick={() => {
             onFocus();
@@ -154,10 +154,10 @@ function FormFieldRenderer() {
             // optionally, an object with props that will be passed to the dialog.
             // These props will be returned by the useUiExtensionDialog hook
             //
-            openDialog<DialogReturn, DialogProps>("/cloudinary", {
+            openDialog<DialogReturn, DialogProps>('/cloudinary', {
               // By default, native GraphCMS dialogs have a maxWidth of 600px.
               // You can overwrite it by passing a maxWidth prop
-              maxWidth: "90vw",
+              maxWidth: '90vw',
               isList,
               media,
               // openDialog returns a Promise with the value that was passed to onCloseDialog
@@ -177,9 +177,9 @@ function FormFieldRenderer() {
           media.map((item, index) => (
             <div
               style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "flex-start",
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'flex-start',
                 opacity:
                   isExpanded && item.public_id !== clickedMedia?.public_id
                     ? 0
@@ -207,25 +207,25 @@ function FormFieldRenderer() {
               />
               <button
                 style={{
-                  marginLeft: "10px",
-                  cursor: "pointer",
-                  boxSizing: "border-box",
-                  userSelect: "none",
-                  color: "#6663FD",
-                  backgroundColor: "transparent",
-                  textAlign: "center",
-                  lineHeight: "14px",
-                  display: "inline-flex",
-                  border: "0px",
+                  marginLeft: '10px',
+                  cursor: 'pointer',
+                  boxSizing: 'border-box',
+                  userSelect: 'none',
+                  color: '#6663FD',
+                  backgroundColor: 'transparent',
+                  textAlign: 'center',
+                  lineHeight: '14px',
+                  display: 'inline-flex',
+                  border: '0px',
                   fontWeight: 500,
-                  fontFamily: "sans-serif",
-                  fontSize: "12px",
-                  verticalAlign: "middle",
-                  padding: "4px",
+                  fontFamily: 'sans-serif',
+                  fontSize: '12px',
+                  verticalAlign: 'middle',
+                  padding: '4px',
                 }}
                 onClick={() => {
                   const newValues = media.filter(
-                    (value) => value.public_id !== item.public_id,
+                    (value) => value.public_id !== item.public_id
                   );
                   onChange(newValues);
                 }}
@@ -292,7 +292,7 @@ function TableCellRenderer() {
   return (
     <div
       style={{
-        display: "flex",
+        display: 'flex',
         opacity: isTransitioning ? 0 : 1,
       }}
     >
@@ -363,15 +363,15 @@ function FormAsset({
     );
   }
 
-  if (media.resource_type === "image")
+  if (media.resource_type === 'image')
     return (
       <button
         onClick={onOpen}
         style={{
-          border: "none",
-          outline: "none",
-          background: "transparent",
-          cursor: "pointer",
+          border: 'none',
+          outline: 'none',
+          background: 'transparent',
+          cursor: 'pointer',
         }}
       >
         <Image
@@ -385,7 +385,7 @@ function FormAsset({
       </button>
     );
 
-  if (media.resource_type === "video")
+  if (media.resource_type === 'video')
     return (
       <Video
         cloudName={cloudName}
@@ -428,7 +428,7 @@ function TableAsset({
       tabIndex={0}
       onClick={onOpen}
       onKeyPress={(e) => {
-        if (e.key === "Enter") {
+        if (e.key === 'Enter') {
           onOpen();
         }
       }}
@@ -439,31 +439,31 @@ function TableAsset({
         ...style,
       }}
     >
-      {media.resource_type === "image" ? (
+      {media.resource_type === 'image' ? (
         <Image
           publicId={media.public_id}
           cloudName={cloudName}
           width="auto"
           crop="scale"
           style={{
-            objectFit: "cover",
-            width: "100%",
-            height: "100%",
-            cursor: "pointer",
+            objectFit: 'cover',
+            width: '100%',
+            height: '100%',
+            cursor: 'pointer',
           }}
         >
           <Placeholder />
         </Image>
-      ) : media.resource_type === "video" ? (
+      ) : media.resource_type === 'video' ? (
         <Video
           cloudName={cloudName}
           publicId={media.public_id}
           width="auto"
           style={{
-            objectFit: "cover",
-            width: "100%",
-            height: "100%",
-            cursor: "pointer",
+            objectFit: 'cover',
+            width: '100%',
+            height: '100%',
+            cursor: 'pointer',
           }}
         />
       ) : null}
@@ -501,14 +501,14 @@ function TableAssetsPreviewModal({
       aria-modal="true"
       role="dialog"
       style={{
-        position: "fixed",
+        position: 'fixed',
         inset: 0,
-        display: "flex",
+        display: 'flex',
       }}
       onClick={closeModal}
     >
       <div
-        style={{ margin: "auto" }}
+        style={{ margin: 'auto' }}
         onClick={(e) => {
           e.stopPropagation();
         }}
@@ -516,13 +516,13 @@ function TableAssetsPreviewModal({
         <CloseModalButton closeModal={closeModal} />
         <div
           style={{
-            display: "flex",
-            borderRadius: "4px",
-            backgroundColor: "white",
-            maxWidth: "800px",
-            maxHeight: "400px",
-            padding: "16px",
-            overflowY: "auto",
+            display: 'flex',
+            borderRadius: '4px',
+            backgroundColor: 'white',
+            maxWidth: '800px',
+            maxHeight: '400px',
+            padding: '16px',
+            overflowY: 'auto',
           }}
         >
           {media.map((item, index) => (
@@ -561,13 +561,13 @@ function FullScreenPreview({
 }) {
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
-      if (e.key === "Escape") {
+      if (e.key === 'Escape') {
         onClose();
       }
     };
-    window.addEventListener("keyup", handler);
+    window.addEventListener('keyup', handler);
     return () => {
-      window.removeEventListener("keyup", handler);
+      window.removeEventListener('keyup', handler);
     };
   }, [onClose]);
 
@@ -576,11 +576,11 @@ function FullScreenPreview({
       aria-modal="true"
       role="dialog"
       style={{
-        position: "fixed",
+        position: 'fixed',
         inset: 0,
-        display: "flex",
-        alignItems: "center",
-        overflow: "auto",
+        display: 'flex',
+        alignItems: 'center',
+        overflow: 'auto',
       }}
       onClick={(e) => {
         e.stopPropagation();
@@ -588,22 +588,22 @@ function FullScreenPreview({
       }}
     >
       <CloseModalButton closeModal={onClose} />
-      {media.resource_type === "image" ? (
+      {media.resource_type === 'image' ? (
         <Image
           cloudName={cloudName}
           publicId={media.public_id}
           width="auto"
-          style={{ maxWidth: "100%", margin: "auto", maxHeight: "100%" }}
+          style={{ maxWidth: '100%', margin: 'auto', maxHeight: '100%' }}
           crop="scale"
         >
           <Placeholder />
         </Image>
-      ) : media.resource_type === "video" ? (
+      ) : media.resource_type === 'video' ? (
         <Video
           cloudName={cloudName}
           publicId={media.public_id}
           controls
-          style={{ maxWidth: "70vw", margin: "auto", maxHeight: "100%" }}
+          style={{ maxWidth: '70vw', margin: 'auto', maxHeight: '100%' }}
         />
       ) : null}
     </div>
@@ -624,25 +624,24 @@ function CloudinaryDialog() {
     // @ts-expect-error
     const mediaLibrary = window.cloudinary.createMediaLibrary(
       {
-        // REMOVE THIS!!!!
-        cloud_name: "dzpcxb0i9",
-        api_key: "247126528389283",
+        cloud_name: config.CLOUD_NAME,
+        api_key: config.API_KEY,
         remove_header: false,
-        max_files: "10",
+        max_files: '10',
         multiple: isList,
-        insert_caption: "Add & close",
-        inline_container: "#cloudinary_dialog",
+        insert_caption: 'Add & close',
+        inline_container: '#cloudinary_dialog',
         default_transformations: [
-          [{ quality: "auto" }, { fetch_format: "auto" }],
+          [{ quality: 'auto' }, { fetch_format: 'auto' }],
           [
             {
               width: 80,
               height: 80,
-              crop: "fill",
-              gravity: "auto",
-              radius: "max",
+              crop: 'fill',
+              gravity: 'auto',
+              radius: 'max',
             },
-            { fetch_format: "auto", quality: "auto" },
+            { fetch_format: 'auto', quality: 'auto' },
           ],
         ],
       },
@@ -669,7 +668,7 @@ function CloudinaryDialog() {
             onCloseDialog(data.assets[0]);
           }
         },
-      },
+      }
     );
     mediaLibrary.show();
   }, [config.API_KEY, config.CLOUD_NAME, isList, media, onCloseDialog]);
@@ -678,7 +677,7 @@ function CloudinaryDialog() {
     <div
       id="cloudinary_dialog"
       style={{
-        height: "800px",
+        height: '800px',
       }}
     />
   );
@@ -689,23 +688,23 @@ function CloseModalButton({ closeModal }: { closeModal: () => void }) {
     <button
       type="button"
       style={{
-        cursor: "pointer",
-        userSelect: "none",
-        backgroundColor: "transparent",
-        textAlign: "center",
-        lineHeight: "16px",
-        display: "inline-flex",
-        border: "0px",
-        position: "absolute",
-        right: "10px",
-        top: "10px",
+        cursor: 'pointer',
+        userSelect: 'none',
+        backgroundColor: 'transparent',
+        textAlign: 'center',
+        lineHeight: '16px',
+        display: 'inline-flex',
+        border: '0px',
+        position: 'absolute',
+        right: '10px',
+        top: '10px',
         fontWeight: 500,
-        background: "white",
-        color: "black",
-        fontSize: "16px",
-        borderRadius: "4px",
-        padding: "4px 8px",
-        verticalAlign: "middle",
+        background: 'white',
+        color: 'black',
+        fontSize: '16px',
+        borderRadius: '4px',
+        padding: '4px 8px',
+        verticalAlign: 'middle',
         margin: 0,
       }}
       onClick={(event) => {
@@ -716,7 +715,7 @@ function CloseModalButton({ closeModal }: { closeModal: () => void }) {
       onKeyPress={(event) => {
         event.preventDefault();
         event.stopPropagation();
-        if (event.key === "Enter") {
+        if (event.key === 'Enter') {
           closeModal();
         }
       }}
