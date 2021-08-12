2d3bb74739563d67b7bf7543d1648041407ac875
Jamie Barton <jamie@notrab.dev>
2021-08-12 12:02:56 +0100

    feat: add urql examples

---
+++ with-urql-core/README.md
@@ -0,0 +1,33 @@
+# GraphCMS ⨯ urql
+
+[Join our Slack](https://slack.graphcms.com)
+
+This example demonstrates how to query from GraphCMS with core [urql](https://formidable.com/open-source/urql/) client.
+
+• [Demo](https://graphcms-with-urql-core.vercel.app/)
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/import/project?template=https://github.com/GraphCMS/graphcms-examples/tree/master/with-urql-core)
+
+## How to Use
+
+### Download Manually
+
+```bash
+npx degit graphcms/graphcms-examples/with-urql-core with-urql-core
+```
+
+Install & Run:
+
+```bash
+cd with-urql-core
+npm install
+npm run dev
+# or
+cd with-urql-core
+yarn
+yarn dev
+```
+
+### Run on Codesandbox
+
+[![Develop with Codesandbox](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/GraphCMS/graphcms-examples/tree/master/with-urql-core)
---
+++ with-urql-core/pages/products/[slug].js
@@ -0,0 +1,65 @@
+import { Fragment } from 'react';
+import { createClient, gql } from 'urql';
+
+const client = createClient({
+  url: 'https://api-eu-central-1.graphcms.com/v2/ck8sn5tnf01gc01z89dbc7s0o/master',
+});
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+
+  const ProductBySlugQuery = gql`
+    query GetProductBySlug($slug: String!) {
+      product(where: { slug: $slug }) {
+        name
+        description
+        price
+      }
+    }
+  `;
+
+  const {
+    data: { product },
+  } = await client
+    .query(ProductBySlugQuery, {
+      slug,
+    })
+    .toPromise();
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const ProductSlugsQuery = gql`
+    {
+      products {
+        slug
+      }
+    }
+  `;
+
+  const {
+    data: { products },
+  } = await client.query(ProductSlugsQuery).toPromise();
+
+  return {
+    paths: products.map(({ slug }) => ({
+      params: { slug },
+    })),
+    fallback: false,
+  };
+}
+
+export default function ProductPage({ product }) {
+  return (
+    <Fragment>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p>{product.price / 100}</p>
+    </Fragment>
+  );
+}
