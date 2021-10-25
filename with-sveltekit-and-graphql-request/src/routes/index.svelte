d8109a89372ded40ae2e41545a59a2e3ce0ab302
Scott Spence <spences10apps@gmail.com>
2021-10-25 12:28:57 +0100

    feat: ✨ add with graphql request and svelte example (#214)

---
+++ with-sveltekit-and-graphql-request/src/routes/index.svelte
@@ -0,0 +1,42 @@
+<script context="module">
+  import { GraphQLClient } from 'graphql-request'
+  export async function load() {
+    const graphcms = new GraphQLClient(
+      'https://api-eu-central-1.graphcms.com/v2/ck8sn5tnf01gc01z89dbc7s0o/master',
+      {
+        headers: {},
+      }
+    )
+    const { products } = await graphcms.request(
+      `{ 
+        products {
+          slug
+          name
+        }
+      }`
+    )
+    return {
+      props: {
+        products,
+      },
+    }
+  }
+</script>
+
+<script>
+  export let products
+</script>
+
+<svelte:head>
+  <title>SvelteKit project template</title>
+</svelte:head>
+
+<h1>GraphCMS with SvelteKit!</h1>
+
+{#each products as product}
+  <p>
+    <a href="/product/{product.slug}">
+      {product.name}
+    </a>
+  </p>
+{/each}
