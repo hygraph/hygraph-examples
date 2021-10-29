ebdf43316ae3f0d8772e21276edd06df790c8ccd
Scott Spence <spences10apps@gmail.com>
2021-10-29 13:50:04 +0100

    Update with-eleventy/src/_data/graphql.js
    Co-authored-by: Jamie Barton <jamie@notrab.dev>

--- with-eleventy/src/_data/graphql.js
+++ with-eleventy/src/_data/graphql.js
@@ -4,7 +4,6 @@ module.exports = async () => {
     'https://api-eu-central-1.graphcms.com/v2/ck8sn5tnf01gc01z89dbc7s0o/master'
   );
 
-  const { gql } = require('graphql-request');
 
   const query = gql`
     query Products {
