40c1613187428d390f32ff60637811af51d491be
spences10 <spences10apps@gmail.com>
2021-11-01 19:04:20 +0000

    refactor: :recycle: fixes: https://github.com/GraphCMS/graphcms-examples/pull/218#discussion_r739041345

--- with-eleventy/src/_data/graphql.js
+++ with-eleventy/src/_data/graphql.js
@@ -1,9 +1,8 @@
 module.exports = async () => {
-  const { GraphQLClient } = require('graphql-request');
+  const { GraphQLClient, gql } = require('graphql-request');
   const client = new GraphQLClient(
     'https://api-eu-central-1.graphcms.com/v2/ck8sn5tnf01gc01z89dbc7s0o/master'
   );
-
 
   const query = gql`
     query Products {
