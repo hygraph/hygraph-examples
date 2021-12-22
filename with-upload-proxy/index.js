e60cc1d0075ecd9b6f3be3a8c33922a569a89d31
Jamie Barton <jamie@notrab.dev>
2021-12-22 15:59:55 +0000

    feat: upload proxy example

---
+++ with-upload-proxy/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const FormData = require('form-data');
+const fetch = require('node-fetch');
+const multer = require('multer')();
+
+const app = express();
+
+app.post('/upload', multer.single('fileUpload'), (req, res) => {
+  const fileUpload = req.file;
+
+  const form = new FormData();
+
+  form.append('fileUpload', fileUpload.buffer, fileUpload.originalname);
+
+  fetch(`${process.env.GRAPHCMS_ENDPOINT}/upload`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+    },
+    body: form,
+  })
+    .then((response) => response.json())
+    .then((data) => res.send(data));
+});
+
+app.listen(4000, () => {
+  console.log('Running on port 4000');
+});
