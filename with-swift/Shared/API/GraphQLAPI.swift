f96611b3592e8db7bde3ebda7a14c805c6afbdb6
Craig Tweedy <ahoy@craigtweedy.co.uk>
2022-01-08 23:13:21 +0000

    Remove unneeded method

--- with-swift/Shared/API/GraphQLAPI.swift
+++ with-swift/Shared/API/GraphQLAPI.swift
@@ -39,27 +39,12 @@ struct GraphQLOperation<Output: Decodable>: Encodable {
 
 class GraphQLAPI {
     
-    private func performCall(urlRequest: URLRequest) async throws -> (Data, URLResponse) {
-        try await withCheckedThrowingContinuation { continuation in
-            let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-                guard let data = data, let response = response else {
-                    let error = error ?? URLError(.badServerResponse)
-                    return continuation.resume(throwing: error)
-                }
-
-                continuation.resume(returning: (data, response))
-            }
-
-            task.resume()
-        }
-    }
-    
     func performOperation<Output>(_ operation: GraphQLOperation<Output>) async throws -> Output {
         let request: URLRequest
         
         request = try operation.getURLRequest()
 
-        let (data, _) = try await self.performCall(urlRequest: request)
+        let (data, _) = try await URLSession.shared.getData(from: request)
         
         let result = try JSONDecoder().decode(GraphQLResult<Output>.self, from: data)
         guard let object = result.object else {
