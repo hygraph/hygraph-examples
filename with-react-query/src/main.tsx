894e87f3c5f7cfdf001473e379094cced13b8498
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2022-05-03 10:41:49 -0300

    feat(examples): added react query examples

---
+++ with-react-query/src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
