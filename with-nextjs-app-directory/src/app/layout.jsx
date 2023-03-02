95b435a16beefac6182d0317f55bb0c6cbab7382
Bryan Robinson <bryanlrobinson@gmail.com>
2023-03-02 13:45:16 -0500

    Fixes format issue of layout.jsx

--- with-nextjs-app-directory/src/app/layout.jsx
+++ with-nextjs-app-directory/src/app/layout.jsx
@@ -5,10 +5,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
