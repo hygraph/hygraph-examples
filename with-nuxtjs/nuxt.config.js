829f991b3248670bf70e8969de59a616cc620207
Bryan Robinson <bryanlrobinson@gmail.com>
2023-07-03 08:13:49 -0500

    Upgrades Nuxt example to Nuxt3 (#262)
    * Upgrades to nuxt 3
    
    * Cleanup
    
    * Removes config and cleans node scripts
    
    * Removes extaneous comments

--- with-nuxtjs/nuxt.config.js
+++
@@ -1,3 +0,0 @@
-export default {
-  plugins: ['~/plugins/hygraph.js'],
-};
--- with-nuxtjs/plugins/hygraph.js
+++
@@ -1,9 +0,0 @@
-import { GraphQLClient } from 'graphql-request';
-
-const hygraphClient = new GraphQLClient(
-  'https://api-eu-central-1.hygraph.com/v2/ck8sn5tnf01gc01z89dbc7s0o/master'
-);
-
-export default (_, inject) => {
-  inject('hygraph', hygraphClient);
-};
