8c539289c06296dbbf4f89c2161ac147e329eda1
Obie <obiemunozjr@gmail.com>
2023-11-02 16:44:00 -0400

    Updated to latest svelte/kit/vite

---
+++ with-sveltekit/src/routes/product/[...slug]/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+	const product = data.product;
+</script>
+
+<svelte:head>
+	<title>{product.name}</title>
+</svelte:head>
+
+<h1>{product.name}</h1>
+<p>{product.description}</p>
+<p>${product.price / 100}</p>
+
+<style>
+	h1,
+	p {
+		text-align: center;
+		margin: 0 auto;
+	}
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+	p {
+		margin: 1em auto;
+	}
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
--- with-sveltekit/svelte.config.js
+++ with-sveltekit/svelte.config.js
@@ -1,12 +1,13 @@
-/** @type {import('@sveltejs/kit').Config} */
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
-    adapter: vercel(),
-  },
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
 
 export default config;
