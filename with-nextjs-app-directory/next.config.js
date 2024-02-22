d2ab38ec73c7867910851238eb12293c53e485b9
lowisren <lo.etheridge@hygraph.com>
2024-02-22 13:57:37 +0100

    update mgmt sdk links

--- with-nextjs-app-directory/next.config.js
+++ with-nextjs-app-directory/next.config.js
@@ -1,8 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-};
-
 module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['media.graphassets.com'],
+  },
+  experimental: {
+    workerThreads: false,
+    cpus: 1
+  },
+}
